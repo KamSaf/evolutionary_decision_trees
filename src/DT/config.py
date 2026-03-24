4ef8ad6bbdf2f492d54b971e1a19975304939446
KamSaf <bopaf42@gmail.com>
2026-03-24 12:52:44 +0100

    Add seed to decision trees config file

--- src/DT/config.py
+++ src/DT/config.py
@@ -1,8 +1,11 @@
 from pathlib import Path
+from random import seed
 
 ROOT = Path(__file__).parent.parent
 DECISION_COLUMN_SYMBOL = "d"
 DATA_FILE_PATH = f"{ROOT}/data/wdbc.data"
 OUTPUT_PATH = f"{ROOT}/tree.txt"
 INDENT = "      "
-TEST_DATA_RATIO = 0.3
+SEED = 123
+
+seed(SEED)
