a53ff9985aca3fcecde65a564838b4f08e0c521f
KamSaf <bopaf42@gmail.com>
2026-03-25 12:25:42 +0100

    Delete unneccesary constant from config.py

--- src/DT/config.py
+++ src/DT/config.py
@@ -4,7 +4,6 @@ from random import seed
 ROOT = Path(__file__).parent.parent
 DECISION_COLUMN_SYMBOL = "d"
 DATA_FILE_PATH = f"{ROOT}/data/wdbc.data"
-OUTPUT_PATH = f"{ROOT}/tree.txt"
 INDENT = "      "
 SEED = 123
 
